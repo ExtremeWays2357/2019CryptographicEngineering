f79e182bab19063e277df9f077109807f4398a51
Noël Bangma <noel@familiebangma.nl>
2019-02-20 16:19:17 +0100

    New assembly file which works, updated makefile

--- chacha20/chacha20.h
+++ chacha20/chacha20.h
@@ -4,6 +4,10 @@
 #define CHACHA20_KEYBYTES 32
 #define CHACHA20_NONCEBYTES 8
 
+
+
 int crypto_stream_chacha20(unsigned char *c,unsigned long long clen, const unsigned char *n, const unsigned char *k);
+extern void quarterround2(uint32_t *a, uint32_t *b, uint32_t *c, uint32_t *d);
+
 
 #endif
--- chacha20/quarterround.s
+++ chacha20/quarterround.s
@@ -1,24 +1,24 @@
+.syntax unified
+.cpu cortex-m4
 
+.global quarterround2
+.type quarterround2, %function
+quarterround2:
+    # Remember the ABI: we must not destroy the values in r4 to r12.
+    # Arguments are placed in r0 and r1, the return value should go in r0.
+    # To be certain, we just push all of them onto the stack.
+    push {r4-r12}
 
-.text
-.align 4
-.global permutation
-.type permutation STT_FUNC
+    mov r2, r0
+    mov r0, #1
+    loop:
+        cmp r1, #0
+        beq done
+        mul r0, r2
+        sub r1, #1
+        b loop
+    done:
 
-#define globals below
-
-#function definition
-permutation:
-#push regs here
-
-._looptop
-#begin loop
-
-
-.bne _looptop
-
-
-#pop regs
-
-#return with the link register:
+    # Finally, we restore the callee-saved register values and branch back.
+    pop {r4-r12}
 bx lr
