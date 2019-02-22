8c58b427cda4196d5c8a9952e2c4985ea3ad7a95
Noël <noel@familiebangma.nl>
2019-02-22 23:18:08 +0100

    We can do rotates, so lets rotate

--- chacha20/quarterround.s
+++ chacha20/quarterround.s
@@ -39,26 +39,31 @@ quarterround2:
 
 #According to conventions, r0 through r3 contain the arguments of the function call (so a, b, c and d respectively)
 #ARM does not contain a rotate-left instruction, so we simulate using a LSL of X and an LSR of 32-X. 
+#Noël was being stupid, we can just rotate with 32-X instead of doing annoying shifts. Not used to having rotate instructions that are useful
    add r0, r0, r1
    eor r3, r3, r0
-   lsl r5, r3, #16
-   eor r3, r5, r3, lsr #16
+   ror r3, r3, 16
+   #lsl r5, r3, #16
+   #eor r3, r5, r3, lsr #16
    #could rotate ^ but for now, lets just keep shifting until it we have no regression faults.
    
    add r2, r2, r3
    eor r1, r1, r2
-   lsl r5, r1, #12
-   eor r1, r5, r1, lsr #20 
+   ror r1, r1, #20
+   #lsl r5, r1, #12
+   #eor r1, r5, r1, lsr #20 
 
    add r0, r0, r1
    eor r3, r3, r0
-   lsl r5, r3, #8
-   eor r3, r5, r3, lsr #24
+   ror r3, r3, #24
+   #lsl r5, r3, #8
+   #eor r3, r5, r3, lsr #24
 
    add r2, r2, r3
    eor r1, r1, r2
-   lsl r5, r1, #7
-   eor r1, r5, r1, lsr #25
+   ror r1, r1, #25
+   #lsl r5, r1, #7
+   #eor r1, r5, r1, lsr #25
 
    # Finally, we restore the callee-saved register values and branch back.
     pop {r4-r12}
