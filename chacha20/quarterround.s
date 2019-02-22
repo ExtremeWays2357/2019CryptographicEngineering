40fdd7098f3b0922908d883590b7039a62185bef
Noël Bangma <noel@familiebangma.nl>
2019-02-22 20:36:41 +0100

    more assembly changes, ready to merge and branch again

--- chacha20/quarterround.s
+++ chacha20/quarterround.s
@@ -1,6 +1,17 @@
 .syntax unified
 .cpu cortex-m4
 
+.data
+#some globals, for example:
+#somedata:
+# .doubleword 0x123456789abcdef
+# .word 0x12345678
+# .halfword 0x1234
+# .byte 0x2a
+# .nibble 0xf
+
+
+.text
 .global quarterround2
 .type quarterround2, %function
 quarterround2:
@@ -8,6 +19,25 @@ quarterround2:
     # Arguments are placed in r0 and r1, the return value should go in r0.
     # To be certain, we just push all of them onto the stack.
     push {r4-r12}
+
+    #Execute code from quarterround function, merge later:
+
+#  *a = *a + *b;
+#  *d = *d ^ *a;
+#  *d = rotate(*d, 16);
+
+#  *c = *c + *d;
+#  *b = *b ^ *c;
+#  *b = rotate(*b, 12);
+
+#  *a = *a + *b;
+#  *d = *d ^ *a;
+#  *d = rotate(*d, 8);
+
+#  *c = *c + *d;
+#  *b = *b ^ *c;
+#  *b = rotate(*b, 7);
+
 
     mov r2, r0
     mov r0, #1
